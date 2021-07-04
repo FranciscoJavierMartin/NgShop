1c55267899018b8b2dc984eeb04ca8c965caca8f
Francisco <javiermartincuenca@gmail.com>
2021-07-04 11:44:03 +0200

    CRUD categories

--- backend/src/models/product.ts
+++ backend/src/models/product.ts
@@ -1,11 +1,60 @@
 import { Schema, model } from 'mongoose';
 
 const productSchema = new Schema({
-  name: String,
-  image: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: '',
+  },
+  image: {
+    type: String,
+    default: '',
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: String,
+    default: '',
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+  },
   countInStock: {
     type: Number,
     required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: 0,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
   },
 });
 
--- backend/src/routers/categories.ts
+++ backend/src/routers/categories.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Request, Router } from 'express';
 import { Category } from '../models/category';
 
 const router = Router();
@@ -10,7 +10,83 @@ router.get('/', async (req, res) => {
     res.status(500).json({ success: false });
   }
 
-  res.send(categoryList);
+  res.status(200).send(categoryList);
+});
+
+router.get('/:id', async (req: Request<{ id: string }>, res) => {
+  const category = await Category.findById(req.params.id);
+
+  if (category) {
+    res.status(200).send(category);
+  } else {
+    res
+      .status(404)
+      .json({ message: 'The category with the given ID was not found' });
+  }
+});
+
+interface CreateCategoryRequestBody {
+  name: string;
+  icon: string;
+  color: string;
+}
+
+router.post(
+  '/',
+  async (req: Request<any, any, CreateCategoryRequestBody>, res) => {
+    const category = new Category({
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    });
+
+    const savedCategory = await category.save();
+
+    if (!savedCategory) {
+      res.status(500).send('Category cannot be created');
+    } else {
+      res.status(201).send(category);
+    }
+  }
+);
+
+interface UpdateCategoryRequestBody {
+  name: string;
+  icon: string;
+  color: string;
+}
+
+router.put(
+  '/:id',
+  async (req: Request<{ id: string }, any, UpdateCategoryRequestBody>, res) => {
+    const updatedCategory = await Category.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    });
+
+    if (updatedCategory) {
+      res.send(updatedCategory);
+    } else {
+      res.status(400).send('Category cannot be updated');
+    }
+  }
+);
+
+router.delete('/:id', async (req: Request<{ id: string }>, res) => {
+  try {
+    const deletedCategory = await Category.findByIdAndRemove(req.params.id);
+
+    if (deletedCategory) {
+      res.status(200).json({ success: true, message: 'Category removed' });
+    } else {
+      res
+        .status(404)
+        .json({ success: false, message: 'Category has not been removed' });
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
 });
 
 export default router;
